04c5037213161ad5783deec89eef422489864b97
bodaisatuta <usman.nagasaki@gmail.com>
2024-02-14 08:58:10 +0900

    application.py

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    if request.method == 'GET':
+        text = 'ここに結果が出力されます'
+        return render_template('page.html', text=text)
+    elif request.method == 'POST':
+        name = request.form['name']
+        text = 'こんにちは' + name + 'さん'
+        return render_template('page.html', text=text)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
